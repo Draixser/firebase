0de26483b40ab7ebbba69b038b9181c02e906118
Draixser <draixser@gmail.com>
2024-11-12 11:44:47 -0500

    uso del userForm

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,12 +1,31 @@
-import React,{useContext,useState} from 'react'
-import { UserContext } from '../context/UserProvider'
+import React, { useContext, useState } from "react";
+import { UserContext } from "../context/UserProvider";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
-    const [email,setEmail]= useState("emgallego@uao.edu.co");
-    const [password,setPasword]= useState("q1234567890");
-    const {registerUser} =useContext(UserContext);
+  // const [email,setEmail]= useState("emgallego@uao.edu.co");
+  // const [password,setPasword]= useState("q1234567890");
+  const { registerUser } = useContext(UserContext);
+  const navegate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm();
 
-    const handleSubmit=async(e)=>{
+  const onSubmmit = async ({ email, password }) => {
+    console.log("procesando formulario--->_ ", email, password);
+    try {
+      await registerUser(email, password);
+      navegate("/");
+    } catch (error) {
+      console.log(error.code);
+    }
+  };
+
+  /* const handleSubmit=async(e)=>{
         e.preventDefault();//previene recargar  todo el sitio
         console.log("enviando datos: ",email," ",password)
         try {
@@ -19,20 +38,80 @@ const Register = () => {
 
 
 
-    }
+    }*/
 
   return (
-
     <>
-    <div>Register</div>
-    <form  onSubmit={handleSubmit}>
-        <input type="email" name="" id="" placeholder='email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
-        <input type="password" name="" id=""placeholder='password' value={password} onChange={(e)=>setPasword(e.target.value)} />
-        <button type="submit">Enviar</button>
-    </form>
-    </>
-    
-  )
-}
+      <div>Register</div>
+      <form onSubmit={handleSubmit(onSubmmit)}>
+        <input
+          type="email"
+          name=""
+          id=""
+          placeholder="email"
+          {...register("email", {
+            required: {
+              value: true,
+              message: " el campo es obligatorio",
+            },
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "formato de email incorrecto",
+            },
+          })}
+          /*value={email} 
+        onChange={(e)=>setEmail(e.target.value)}*/
+        />
 
-export default Register
+        {errors.email && <p>{errors.email.message}</p>}
+        <input
+          type="password"
+          name=""
+          id=""
+          placeholder="password"
+          {...register("password", {
+            required: {
+              value: true,
+              message: "la contraseña es obligatoria",
+            },
+            minLength: {
+              value: 8,
+              message: "la contraseña debe tener almenos 8 caracteres",
+            },
+            validate: {
+              trim: (v) => {
+                if (!v.trim()) {
+                  return "no seas 🤬, escribe algo";
+                }
+                return true;
+              },
+            },
+          })}
+          /* value={password} 
+        onChange={(e)=>setPasword(e.target.value)} */
+        />
+         {errors.password && <p>{errors.password.message}</p>}
+        <input
+          type="password"
+          name=""
+          id=""
+          placeholder="password2"
+          {...register("password2", {
+            required: "la contraseña es obligatoria",
+            validate: {
+              equals: (v) => {
+                v === getValues("password") || "no coincide las contraseñas";
+              },
+            },
+          })}
+          /* value={password} 
+        onChange={(e)=>setPasword(e.target.value)} */
+        />
+        {errors.password2 && <p>{errors.password2.message}</p>}
+        <button type="submit">Enviar</button>
+      </form>
+    </>
+  );
+};
+
+export default Register;
